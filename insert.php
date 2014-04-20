0950d759e8807d63e5c863602fac62b9689ab69b
Nishant <nishant_cs@hotmail.com>
2014-04-20 09:34:07 +0530

    File arrangement
    Files are arranged according to the technology.

--- insert.php
+++
@@ -1,7 +0,0 @@
-
-<form name="test" action="insertthis.php" method="post">
-Enter Longitude <input type="text" name="long"><br>
-Enter Latitude <input type="text" name="lat"><br>
-<input type="submit" value="submit">
-
-</form>
--- test.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-document.write("<?php 
-	echo 'hello nishant into php';
-	?>");
-</script>
